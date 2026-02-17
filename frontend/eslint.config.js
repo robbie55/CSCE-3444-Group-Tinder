7d920215cc9067bfb64cdf9b91d0637daa8e183a
chris940dtx <christophertex1@gmail.com>
2026-02-17 14:01:08 -0600

    Added users router and updated readme for server run directions

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
     globalIgnores(['dist']),
